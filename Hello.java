d0ca55268909a9b7ce4d8aa4b3523b2983651fb6
hujianfei1989 <5319316763@qq.com>
2015-10-16 22:11:21 +0800

    Update Hello.java File .Add two lines

--- Hello.java
+++ Hello.java
@@ -1,4 +1,6 @@
 public class Hello {
 	public static void main(String args[]) {
-	System.out.println("Hello world !")};
+		System.out.println("Hello world !")};
+		System.out.println("Hello Jef");
+		System.out.println("Hello 123");
 }
