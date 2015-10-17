4598a62572fdcd11184f912ff71b460f0f02ad15
hujianfei1989 <5319316763@qq.com>
2015-10-17 13:04:52 +0800

    Master modified

--- Hello.java
+++ Hello.java
@@ -3,4 +3,6 @@ public class Hello {
 		System.out.println("Hello world !")};
 		System.out.println("Hello Jeffrey21");
 		System.out.println("Hello Myself");
+		System.out.println("Hello Myself2345");
+		
 }
